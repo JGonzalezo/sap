8669834fdd7d00d51afbc10d22f1d7c01e871ce5
bluRyze <javizz.hjsg@gmail.com>
2016-10-02 12:09:17 -0700

    logistica.php
    Menu principal de logistica

---
+++ logistica.php
@@ -0,0 +1,223 @@
+<?php include('header.php') ?>
+<section class="container">
+	<section class="main">
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Alumnos</h3>
+				</div>
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="altaAlumno.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+
+			<div class="row">
+			</div>
+
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Clases</h3>
+				</div>
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="altaClases.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+
+			<div class="row">
+			</div>
+
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Materias</h3>
+				</div>
+
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="altaMateria.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+
+			<div class="row">
+			</div>
+
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Carreras</h3>
+				</div>
+
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="altaCarrera.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+
+			<div class="row">
+			</div>
+
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Horarios</h3>
+				</div>
+
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="altaHorario.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+
+			<div class="row">
+			</div>
+
+			<div class="panel panel-primary" id="panelWrap">
+				<div class="panel-heading">
+					<h3 class="panel-title">Aulas</h3>
+				</div>
+
+				<div class="panelContent">
+					<span class="panelItems">
+						<a href="almoAlta.php">
+							<button type="button" class="btn btn-default btn-lg">
+								<span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true"></span> Altas
+							</button>
+						</a>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-circle-arrow-down" aria-hidden="true"></span> Bajas
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span> Consulta
+						</button>
+					</span>
+
+					<span class="panelItems">
+						<button type="button" class="btn btn-default btn-lg">
+							<span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Reportes
+						</button>
+					</span>
+				</div>
+			</div>
+		</div>
+		</section>
+	</section>
+<?php include('footer.php') ?>
